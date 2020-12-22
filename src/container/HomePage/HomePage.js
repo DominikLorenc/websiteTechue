947dddf9d1d051f84600992d7d887f8c457b3543
Dominik Lorenc <dominiklorenc1270@gmail.com>
2020-12-22 12:35:43 +0100

    Created section Highlights

--- src/container/HomePage/HomePage.js
+++ src/container/HomePage/HomePage.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navigation from "../../components/Navigation/Navigation";
+import Highlights from "../../components/Highlights/Highlights";
 import styled from "./HomePage.module.scss";
 
 const HomePage = (props) => {
   return (
     <div>
       <Navigation />
+      <Highlights />
     </div>
   );
 };
