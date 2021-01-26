f534a3c12c1a61b14c82aef692701eda8a90d6a6
Dominik Lorenc <dominiklorenc1270@gmail.com>
2021-01-26 19:34:49 +0100

    Used componet TitleSection in PartnerSection

--- src/components/Partners/Partners.js
+++ src/components/Partners/Partners.js
@@ -1,18 +1,18 @@
 import React from "react";
 import Gallery from "./Carousel/Carousel";
 import styled from "./Partners.module.scss";
+import TitleSection from "../UI/TitleSection/TitleSection";
 
 const Partners = (props) => {
   return (
     <section className={styled.Partners}>
-      <div>
-        <p>We work with the best</p>
-        <h2>Our Partners</h2>
-        <p>
-          We work with some of the greatest companies in the industry to help
-          deliver our services around the world
-        </p>
-      </div>
+      <TitleSection
+        subtitle={"We work with the best"}
+        title={"Our Partners"}
+        description={
+          "We work with some of the greatest companies in the industry to help deliver our services around the world"
+        }
+      />
       <div className={styled.Carousel}>
         <Gallery />
       </div>
