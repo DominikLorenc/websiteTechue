6007514fa40fff6437ecf3a931daca3df7c304f1
Dominik Lorenc <dominiklorenc1270@gmail.com>
2021-03-02 02:22:32 +0100

    Created section ReviewSection

---
+++ src/components/ReviewSection/GalleryCard/GalleryCard.js
@@ -0,0 +1,17 @@
+import React from "react";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+import "./GalleryCard.scss";
+import ReviewCard from "../ReviewCard/ReviewCard";
+
+const GalleryCard = ({ clients }) => {
+  const item = clients.map(({ id,src,name,proffesion,description }) => <ReviewCard key={id} src={src} name={name} proffesion={proffesion} description={description} />);
+
+  return (
+    <AliceCarousel disableButtonsControls={true} mouseTracking={true}>
+     {item}
+    </AliceCarousel>
+  );
+};
+
+export default GalleryCard;
